8e6c3fbd94f868a1710f8eb846c87711d695ab03
dslkumar <dslkumar333@gmail.com>
2018-07-01 14:49:25 +0530

    Test Project commited

---
+++ MavenTestProject/src/main/java/com/test/prog/TestProgram.java
@@ -0,0 +1,15 @@
+package com.test.prog;
+
+public class TestProgram {
+	
+	public static void main(String[] args) {
+		System.out.println("Main Method");
+		testMethod();
+	}
+	
+	public static void testMethod(){
+		System.out.println("Test Method");
+	}
+
+}
+
